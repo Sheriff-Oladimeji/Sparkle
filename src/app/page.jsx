93c1ca8f648d13aadec96c21dd50424dfb8397e7
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-07-11 17:02:30 +0100

    fix: change header color

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,22 +14,21 @@ const page = () => {
     <section className="pt-24 md:pt-0 font-mont">
       <div className="w-[90%] mx-auto">
         <header className="flex flex-col md:flex-row md:items-center  md:gap-[100px]">
-          <div className="flex flex-col gap-3 mx-auto  md:flex-1">
-            <h1 className="text-4xl md:text-5xl font-bold  header  w-full">
+          <div className="flex flex-col gap-3 mx-auto  md:flex-1  w-full md:mt-0 text-center md:text-left">
+            <h1 className="text-3xl  sm:text-4xl md:text-5xl font-bold   w-full  ">
               Unleash Your Creative Potential with our Expertise
             </h1>
-            
-              <p className="font-lato text-lg">
-                Step into a world of limitless possibilities as we unleash the
-                power of creativity to elevate your brand and captivate your
-                audience.
-              </p>
-              <Link href="/portfolio" alt="">
-                <button className="border-none bg-green text-white font-bold p-4 rounded-lg flex items-center gap-4 mt-4">
-                  See Our Works <BsArrowRight size={25} />
-                </button>
-              </Link>
-          
+
+            <p className="font-lato text-lg">
+              Step into a world of limitless possibilities as we unleash the
+              power of creativity to elevate your brand and captivate your
+              audience.
+            </p>
+            <Link href="/portfolio" alt="" >
+              <button className="border-none bg-green text-white font-bold p-4 rounded-lg flex items-center gap-4 mt-4 ">
+                See Our Works <BsArrowRight size={25} />
+              </button>
+            </Link>
           </div>
           <div className="hidden md:block flex-1 ">
             <Image
@@ -42,7 +41,7 @@ const page = () => {
           </div>
         </header>
 
-        <Service/>
+        <Service />
       </div>
     </section>
   );
