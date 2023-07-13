1ec7272dd8ce1b421bbedc287c6eefadf72ab849
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-07-13 01:59:05 +0100

    feat: add databse to utils

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,1 +1,12 @@
 import mongoose from "mongoose";
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+    } catch (error) {
+        throw new Error("Couldn't connect to Mongo")
+    }
+}
+
+export default connectDB
