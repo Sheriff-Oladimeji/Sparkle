a9e3beda211ec79ab5e48a9e18c52cd9181c6688
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-07-13 02:43:03 +0100

    create contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,8 +2,31 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
+    <section className="page">
+      <div className="w-[90%] mx-auto">
+        <div>
+          <h3>GET IN TOUCH</h3>
+          <h1>Do you have a project in your mind?</h1>
+          <div>
+            <p>
+              <span></span>
+              +2348147800112
+            </p>
+
+            <p>
+              <span></span>
+              hello@sparkle.com
+            </p>
+            <p>
+              <span></span>
+              50 Wall Street Suite, 44150 Ohio, United States
+            </p>
+          </div>
+        </div>
+        <form action=""></form>
+      </div>
+    </section>
+  );
 }
 
 export default Contact
