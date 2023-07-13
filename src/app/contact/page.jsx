6f583b0eafd20496714a553b39c4905fd3d984eb
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-07-13 06:16:29 +0100

    fix: fix bugs

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,12 +1,42 @@
 "use client";
-import React from "react";
-
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 export const metadata = {
   title: "Contact us",
   description: "How can we help?",
 };
 const Contact = () => {
-  
+  const [error, setError] = useState(false);
+  const router = useRouter();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const name = e.target[0].value;
+    const email = e.target[1].value;
+    const subject = e.target[2].value;
+    const message = e.target[3].value;
+    if (!name || !email || !subject || !message) {
+      return setError(!error);
+    }
+
+    try {
+      const res = await fetch("/api/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          subject,
+          message,
+        }),
+      });
+      res.status === 201 &&
+        router.push("/contact/success?success=Information has been saved");
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <section className="page md:pt-4">
       <div className="w-[90%] mx-auto flex flex-col md:flex-row text-black items-center">
@@ -29,7 +59,35 @@ const Contact = () => {
             </p>
           </div>
         </div>
-        
+        <form
+          onSubmit={handleSubmit}
+          className="flex-1 flex flex-col w-full gap-4"
+        >
+          <input type="text" name="name" className="input" placeholder="Name" />
+
+          <input
+            type="text"
+            name="email"
+            className="input"
+            placeholder="Email"
+          />
+
+          <input
+            type="text"
+            name="subject"
+            className="input"
+            placeholder="Subject"
+          />
+
+          <textarea
+            name="message"
+            className="input"
+            placeholder="Message"
+          ></textarea>
+          {error && <p className="text-red-600">All input must be filled</p>}
+
+          <button>Submit</button>
+        </form>
       </div>
     </section>
   );
--- src/models/Contact.js
+++ src/models/Contact.js
@@ -1,26 +1,38 @@
 import mongoose from "mongoose";
-import { Schema } from "mongoose";
+const { Schema } = mongoose;
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  subject: {
-    type: String,
-    required: true,
-  },
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    subject: {
+      type: String,
+      required: true,
+    },
 
-  message: {
-    type: String,
-    required: true,
+    message: {
+      type: String,
+      required: true,
+    },
   },
-});
-let Contact = mongoose.model("Contact", contactSchema)
+  {
+    timestamps: true,
+  }
+);
+let Contact 
 
-export default  Contact
+try {
+   Contact   =  mongoose.model("Contact");
+}
+catch{
+  Contact =  mongoose.model("Contact", contactSchema);
+  }
+
+export default Contact;
