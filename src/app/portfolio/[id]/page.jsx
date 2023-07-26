5c054bdbf1034ca3651bba3a1b8ce3ba37b4e4af
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-07-26 16:36:26 +0100

    fix: change styling for each portfolio

--- src/app/portfolio/[id]/page.jsx
+++ src/app/portfolio/[id]/page.jsx
@@ -16,47 +16,20 @@ const PortfolioItem = ({ params }) => {
   const paragraphs = selected.description.split("\n");
   return (
     <section className="page ">
-      <main className="w-[90%] mx-auto my-8">
-        <div className="flex flex-col-reverse md:flex-row justify-between gap-6 items-center">
-          <div className="flex-1 flex  gap-6 flex-col">
-            <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl font-lato">
-              {selected.title}
-            </h1>
-            <p>{paragraphs[0]}</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid aspernatur quos dignissimos veritatis, harum repudiandae magnam! Id libero voluptate quos!</p>
-          </div>
-          <div className="flex-1">
-            <Image
-              alt={selected.title}
-              src={selected.imgUrl}
-              width={300}
-              height={300}
-              className="w-full h-[400px] object-cover md:object-cover rounded-lg"
-            />
-          </div>
+      <main className="flex flex-col gap-28 md:flex-row justify-between w-[90%] mx-auto my-8 items-center">
+        <div className="relative w-full h-[500px] object-cover flex-1">
+          <Image src={selected.imgUrl} alt={selected.title} fill={true}  />
         </div>
-
-        <div className="flex flex-col md:flex-row justify-between mt-16 mb-10">
-          <div className=" md:w-[70%]">
-            <h3 className="text-green text-2xl font-bold font-lato mb-2">
-              {selected.category}
-            </h3>
-            {paragraphs.map((paragraph, index) => (
-              <p key={index} className="mb-4 max-w-2xl">
-                {paragraph}
-              </p>
-            ))}
-          </div>
-          <div className="flex-1  md:w-[30%]">
-            <h3 className="text-2xl font-bold font-lato text-green">Related Tags</h3>
-            <div className="flex flex-col  gap-4 ">
-              {selected.tags.map((item, index) => (
-                <p key={index} className=" font-bold p-2  flex items-center">
-                  {item}
-                </p>
-              ))}
-            </div>
-          </div>
+        <div className="flex flex-col gap-2 flex-1">
+          <h3 className="text-green text-xl font-lato">{selected.category}</h3>
+          <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl font-lato mb-4">
+            {selected.title}
+          </h1>
+          {paragraphs.map((paragraph, index) => (
+            <p key={index} className="max-w-2xl">
+              {paragraph}
+            </p>
+          ))}
         </div>
       </main>
     </section>
--- src/app/portfolio/data.js
+++ src/app/portfolio/data.js
@@ -1,11 +1,10 @@
-
 const data = [
   {
     id: 1,
     title: "Elevate",
     category: "Web Development",
     description:
-      "Elevate is a cutting-edge web development project that combines innovative design with seamless functionality, delivering exceptional user experiences. Our team of skilled developers leverages the latest web technologies to create stunning websites that not only look great but also perform flawlessly.\n\nWith a focus on user-centric design and smooth navigation, Elevate ensures that every interaction with your website leaves a lasting impression. From responsive layouts to custom animations, we pay attention to every detail to create a digital presence that elevates your brand.\n\nOur web development process starts with a thorough analysis of your business objectives and target audience. We then proceed to wireframing and prototyping to ensure a solid foundation for your website.\n\nOur team carefully crafts the user interface, leveraging intuitive navigation and visually appealing designs. We implement clean and maintainable code, following industry best practices and ensuring scalability for future growth.\n\nWith Elevate, you can expect an optimized website that loads quickly, adapts to different devices, and drives conversions. Whether you need an e-commerce platform, a corporate website, or a portfolio site, we have the expertise to bring your vision to life.",
+      "Elevate is a cutting-edge web development project that combines innovative design with seamless functionality, delivering exceptional user experiences. Our team of skilled developers leverages the latest web technologies to create stunning websites that not only look great but also perform flawlessly.\n\nWith a focus on user-centric design and smooth navigation, Elevate ensures that every interaction with your website leaves a lasting impression. From responsive layouts to custom animations, we pay attention to every detail to create a digital presence that elevates your brand.\n\nOur web development process starts with a thorough analysis of your business objectives and target audience. We then proceed to wireframing and prototyping to ensure a solid foundation for your website.",
     imgUrl:
       "https://img.freepik.com/free-vector/web-design-landing-page-template_52683-18357.jpg?w=740&t=st=1689473806~exp=1689474406~hmac=d986e0fb6d6c8999c7ea93e44194515aaf43d4ff62ee8f6b5cad95fd577682b9",
     tags: [
@@ -14,13 +13,14 @@ const data = [
       "User-Centric Design",
       "Web Technologies",
     ],
+    date: "Sep 19, 2020",
   },
   {
     id: 2,
     title: "BrandCraft",
     category: "Branding & Design",
     description:
-      "BrandCraft offers comprehensive branding and design solutions, helping businesses create compelling brand identities and captivating visual assets.\n\nOur team of talented designers and strategists work closely with you to understand your brand's essence and values. We craft unique and memorable brand experiences by developing visually stunning logos, typography, color palettes, and brand guidelines that resonate with your target audience.\n\nAt BrandCraft, we believe that branding goes beyond aesthetics. We delve deep into market research to uncover insights about your industry and competitors. This knowledge guides us in creating a strategic brand foundation that sets you apart.\n\nWe carefully consider every touchpoint, from print materials to digital assets, ensuring a consistent and cohesive brand experience.\n\nOur collaborative process involves regular feedback and iterations, ensuring that the final brand design truly reflects your vision. With BrandCraft, you can establish a strong brand presence, foster brand loyalty, and make a lasting impact on your customers.",
+      "BrandCraft offers comprehensive branding and design solutions, helping businesses create compelling brand identities and captivating visual assets.\n\nOur team of talented designers and strategists work closely with you to understand your brand's essence and values. We craft unique and memorable brand experiences by developing visually stunning logos, typography, color palettes, and brand guidelines that resonate with your target audience.\n\nAt BrandCraft, we believe that branding goes beyond aesthetics. We delve deep into market research to uncover insights about your industry and competitors. This knowledge guides us in creating a strategic brand foundation that sets you apart.\n\nWe carefully consider every touchpoint, from print materials to digital assets, ensuring a consistent and cohesive brand experience.",
     imgUrl:
       "https://img.freepik.com/free-vector/decorative-elements-eco-corporate-identity_1284-10312.jpg?size=626&ext=jpg&uid=R109479038&ga=GA1.1.566638872.1687628438&semt=ais",
     tags: [
@@ -29,13 +29,14 @@ const data = [
       "Visual Branding",
       "Brand Guidelines",
     ],
+    date: "Oct 3, 2020",
   },
   {
     id: 3,
     title: "DigitalUX",
     category: "Digital Product Design",
     description:
-      "DigitalUX specializes in crafting user-centered digital product designs that prioritize usability, aesthetics, and seamless user experiences.\n\nOur team of UX/UI designers conducts in-depth research to understand your target users, their pain points, and behaviors. We create wireframes, interactive prototypes, and high-fidelity designs that are visually appealing and intuitive to use.\n\nOur iterative design process begins with user research and persona development. We then proceed to create user flows and information architecture to ensure a logical and intuitive user journey. Through user testing and feedback, we refine the designs, making data-driven improvements that align with your business goals.\n\nDigitalUX follows best practices in usability and accessibility, ensuring that your digital product is inclusive and easy to navigate.\n\nFrom mobile apps to web platforms, we combine aesthetics and functionality to create engaging user experiences that leave a lasting impression.",
+      "DigitalUX specializes in crafting user-centered digital product designs that prioritize usability, aesthetics, and seamless user experiences.\n\nOur team of UX/UI designers conducts in-depth research to understand your target users, their pain points, and behaviors. We create wireframes, interactive prototypes, and high-fidelity designs that are visually appealing and intuitive to use.\n\nOur iterative design process begins with user research and persona development. We then proceed to create user flows and information architecture to ensure a logical and intuitive user journey. Through user testing and feedback, we refine the designs, making data-driven improvements that align with your business goals.\n\nDigitalUX follows best practices in usability and accessibility, ensuring that your digital product is inclusive and easy to navigate.",
     imgUrl:
       "https://img.freepik.com/free-vector/realistic-ui-ux-landing-page-template_52683-68898.jpg?size=626&ext=jpg&uid=R109479038&ga=GA1.2.566638872.1687628438&semt=ais",
     tags: [
@@ -44,13 +45,14 @@ const data = [
       "Usability Testing",
       "Information Architecture",
     ],
+    date: "Nov 15, 2020",
   },
   {
     id: 4,
     title: "ResponsiveX",
     category: "Web Development",
     description:
-      "ResponsiveX specializes in creating responsive websites that adapt flawlessly to different devices, providing a consistent and optimized browsing experience.\n\nWe understand the importance of responsive design in today's mobile-first world, where users access websites from various devices and screen sizes.\n\nOur team begins by designing mobile-first experiences, ensuring that your website looks and functions beautifully on smartphones and tablets. We then progressively enhance the design to deliver a seamless experience on larger screens.\n\nWe carefully consider touch gestures, device capabilities, and load times to create a fast and engaging user experience.\n\nWith ResponsiveX, your website will have fluid layouts, flexible images, and responsive typography that adapts to different screen resolutions.\n\nWe prioritize performance optimization and employ best practices in responsive design to make sure your website reaches and engages users, regardless of the device they use.",
+      "ResponsiveX specializes in creating responsive websites that adapt flawlessly to different devices, providing a consistent and optimized browsing experience.\n\nWe understand the importance of responsive design in today's mobile-first world, where users access websites from various devices and screen sizes.\n\nOur team begins by designing mobile-first experiences, ensuring that your website looks and functions beautifully on smartphones and tablets. We then progressively enhance the design to deliver a seamless experience on larger screens.\n\nWe carefully consider touch gestures, device capabilities, and load times to create a fast and engaging user experience.\n\nWith ResponsiveX, your website will have fluid layouts, flexible images, and responsive typography that adapts to different screen resolutions.",
     imgUrl:
       "https://img.freepik.com/free-psd/american-food-style-stationery_23-2148473308.jpg?w=996&t=st=1689473867~exp=1689474467~hmac=6c5ea839d1567fcf18f0e26516756176bf5330edb807b4cad35d0496afc359b7",
     tags: [
@@ -59,13 +61,14 @@ const data = [
       "Performance Optimization",
       "Fluid Layouts",
     ],
+    date: "Dec 8, 2020",
   },
   {
     id: 5,
     title: "BrandRevive",
     category: "Branding & Design",
     description:
-      "BrandRevive rejuvenates brands by developing creative and impactful brand strategies that reignite customer interest and loyalty.\n\nOur team of brand strategists conducts extensive market research to identify opportunities and gain insights into your target audience. We then develop comprehensive brand strategies that encompass positioning, messaging, visual identity, and brand touchpoints.\n\nAt BrandRevive, we believe that a strong brand narrative is essential for building meaningful connections. We craft compelling stories that resonate with your customers, helping you stand out from the competition.\n\nOur team develops brand guidelines that provide a framework for consistent and cohesive brand communication across various platforms and touchpoints.\n\nWith BrandRevive, your brand will experience a revival, capturing the attention and loyalty of your target audience. Whether you're launching a new brand or revitalizing an existing one, we're here to guide you on your brand journey.",
+      "BrandRevive rejuvenates brands by developing creative and impactful brand strategies that reignite customer interest and loyalty.\n\nOur team of brand strategists conducts extensive market research to identify opportunities and gain insights into your target audience. We then develop comprehensive brand strategies that encompass positioning, messaging, visual identity, and brand touchpoints.\n\nAt BrandRevive, we believe that a strong brand narrative is essential for building meaningful connections. We craft compelling stories that resonate with your customers, helping you stand out from the competition.",
     imgUrl:
       "https://img.freepik.com/free-vector/corporate-identity-black-fire-template-decorative-set-with-paper-bag-tag-mug-vector-illustration_98292-6501.jpg?size=626&ext=jpg&uid=R109479038&ga=GA1.2.566638872.1687628438&semt=ais",
     tags: [
@@ -74,13 +77,14 @@ const data = [
       "Brand Messaging",
       "Visual Identity",
     ],
+    date: "Jan 22, 2021",
   },
   {
     id: 6,
     title: "PixelPerfect",
     category: "Digital Product Design",
     description:
-      "PixelPerfect is dedicated to creating pixel-perfect user interfaces and designs that deliver flawless interactions and engaging visual experiences.\n\nOur meticulous attention to detail ensures that every element, from buttons to icons, is thoughtfully designed and aligned to provide a seamless user experience.\n\nWe begin by understanding your product's requirements and target audience. Our team creates wireframes and low-fidelity prototypes to establish the overall structure and flow. We then move on to designing high-fidelity interfaces, carefully selecting color palettes, typography, and iconography that align with your brand.\n\nPixelPerfect's design process involves user testing and iteration, ensuring that the final product meets the highest standards of usability and aesthetics.\n\nWhether it's a website, mobile app, or software interface, we strive to create visually stunning and intuitive designs that captivate your users.",
+      "PixelPerfect is dedicated to creating pixel-perfect user interfaces and designs that deliver flawless interactions and engaging visual experiences.\n\nOur meticulous attention to detail ensures that every element, from buttons to icons, is thoughtfully designed and aligned to provide a seamless user experience.\n\nWe begin by understanding your product's requirements and target audience. Our team creates wireframes and low-fidelity prototypes to establish the overall structure and flow. We then move on to designing high-fidelity interfaces, carefully selecting color palettes, typography, and iconography that align with your brand.\n\nPixelPerfect's design process involves user testing and iteration, ensuring that the final product meets the highest standards of usability and aesthetics.",
     imgUrl:
       "https://img.freepik.com/free-psd/landing-page-minimal-style-art-gallery-with-man_23-2148821375.jpg?w=826&t=st=1689473939~exp=1689474539~hmac=a74aa7996c3e90013ebdc7a0f6610f75922f3b86c22b3cf6e5f74ad3da20bd2a",
     tags: [
@@ -89,7 +93,8 @@ const data = [
       "Usability Testing",
       "Visual Design",
     ],
+    date: "Feb 14, 2021",
   },
 ];
 
-export default data
+export default data;
