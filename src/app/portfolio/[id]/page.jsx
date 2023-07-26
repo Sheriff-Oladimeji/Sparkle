60e1442da9584d8427cd1461f10d17e4f0011c45
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-07-26 17:00:46 +0100

    feat: add date to portfolio details

--- src/app/portfolio/[id]/page.jsx
+++ src/app/portfolio/[id]/page.jsx
@@ -1,7 +1,9 @@
+"use client";
 import React from "react";
 import data from "../data";
 import { notFound } from "next/navigation";
 import Image from "next/image";
+import { select } from "@material-tailwind/react";
 
 const getData = (portfolio) => {
   const selected = data.find((item) => item.id === parseInt(portfolio));
@@ -16,9 +18,9 @@ const PortfolioItem = ({ params }) => {
   const paragraphs = selected.description.split("\n");
   return (
     <section className="page ">
-      <main className="flex flex-col gap-28 md:flex-row justify-between w-[90%] mx-auto my-8 items-center">
-        <div className="relative w-full h-[500px] object-cover flex-1">
-          <Image src={selected.imgUrl} alt={selected.title} fill={true}  />
+      <main className="flex flex-col gap-10 md:gap-24 md:flex-row justify-between w-[90%] mx-auto my-8 items-center">
+        <div className="relative w-full  h-[400px] sm:h-[500px] object-cover md:flex-1">
+          <Image src={selected.imgUrl} alt={selected.title} fill={true} />
         </div>
         <div className="flex flex-col gap-2 flex-1">
           <h3 className="text-green text-xl font-lato">{selected.category}</h3>
@@ -30,6 +32,11 @@ const PortfolioItem = ({ params }) => {
               {paragraph}
             </p>
           ))}
+
+          <div className="flex items-center gap-2">
+            <h4 className="text-xl font-bold">Date:</h4>
+            <p>{selected.date}</p>
+          </div>
         </div>
       </main>
     </section>
