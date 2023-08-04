35d910704c1633f97f25313c92669e1f74d0a61b
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-08-04 07:04:16 +0100

    feat: add custom favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,14 +2,35 @@ import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
 import Footer from "@/components/footer/Footer";
 
+
 export const metadata = {
   title: "Sparkle",
   description: "Empower your brand with our creative expertise",
+  icons: [
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicon/favicon-16x16.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      url: "/favicon/apple-touch-icon.png",
+    },
+  ],
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+  
       <body className="font-mont flex flex-col justify-between min-h-screen">
         <Navbar/>
         {children}
